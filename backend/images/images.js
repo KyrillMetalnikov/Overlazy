170a9409e886839d37fb78c671d9f8b476917c12
Kyrill Metalnikov <kmetalnikov@hotmail.com>
2022-04-02 01:11:49 -0700

    Finished the images endpoints

--- backend/images/images.js
+++ backend/images/images.js
@@ -22,18 +22,15 @@ app.post('/4537/API/V1/images/', function(req, res) {
 			throw error;
 	});
 
-    let imageLink = req.body.imageLink;
-    let imageYear = req.body.imageYear;
-    let imageMonth = req.body.imageMonth;
-    let imageDay = req.body.imageDay;
-    let imageUserId = req.body.userId;
-
-    const imageDate = new Date(imageYear, imageMonth, imageDay).toJSON().slice(0, 10);
+    const imageLink = req.body.imageLink;
+    const imageDate = req.body.imageDate;
+    const imageUserId = req.body.userId;
 
 	if (imageLink && imageDate && imageUserId) {
-		connection.query("INSERT INTO images VALUES ('?', '?', '?');", [imageUserId, imageDate, imageLink], function(error, results, fields) {
+		connection.query("INSERT INTO images VALUES (?, ?, ?);", [imageUserId, imageDate, imageLink], function(error, results, fields) {
 			if (error) {
-                throw error;
+                res.status(401);
+                res.send("invalid format or image already exists")
             }
 
 			if (results) {
@@ -53,8 +50,10 @@ app.post('/4537/API/V1/images/', function(req, res) {
 
 app.get('/4537/API/V1/images/', function(req, res) {
 	connection.query('UPDATE requests SET req_amount = req_amount + 1 WHERE req_name = "images_get";', function (error, results, fields) {
-		if (error)
-			throw error;
+		if (error){
+		res.status(400);
+		res.send('userId is empty');
+		}
 	});
 
 	let userId = req.query.userId;
@@ -62,7 +61,8 @@ app.get('/4537/API/V1/images/', function(req, res) {
 	if (userId) {
 		connection.query('SELECT * FROM images WHERE images.id = ?;', [userId], function(error, results, fields) {
 			if (error) {
-                throw error;
+                res.status(500);
+				res.send("Unknown server error occured")
             }
 
 			if (results.length > 0) {
@@ -80,38 +80,94 @@ app.get('/4537/API/V1/images/', function(req, res) {
 	}
 });
 
-// app.patch('/4537/API/V1/images/', function(req, res) {
-// 	connection.query('UPDATE requests SET req_amount = req_amount + 1 WHERE req_name = "images_post";', function (error, results, fields) {
-// 		if (error)
-// 			throw error;
-// 	});
+app.delete("/4537/API/V1/images/", function(req, res) {
+	connection.query('UPDATE requests SET req_amount = req_amount + 1 WHERE req_name = "images_delete";', function (error, results, fields) {
+		if (error)
+			throw error;
+	});
 
-// 	let imageLink = req.body.imageLink;
-//     let imageDate = req.body.imageDate;
-//     let imageUserId = req.body.userId;
+	const imageLink = req.body.imageLink;
+    const imageDate = req.body.imageDate;
+    const imageUserId = req.body.userId;
 
-// 	if ((imageLink || imageDate) && imageUserId) {
-//         if (imageLink) {
-//             connection.query('UPDATE images SET (images_link = ? AND images_date = ? AND id = ?);', [imageLink, imageDate, imageUserId], function(error, results, fields) {
-//                 if (error) {
-//                     throw error;
-//                 }
+	if (imageLink && imageDate && imageUserId) {
+		connection.query("DELETE FROM images WHERE images.id=? AND images_date=? AND images_link=?;", [imageUserId, imageDate, imageLink], function(error, results, fields) {
+			if (error) {
+                res.status(401);
+                res.send("invalid format or image doesn't exist");
+				return;
+            }
 
-//                 if (results.length == 1) {
-//                     res.status(200);
-//                     res.send('login successful');
-//                 } else {
-//                     res.status(401);
-//                     res.send('login failed');
-//                 }
+			if (results) {
+				res.status(200);
+				res.send('Success');
+			} else {
+                res.status(401);
+                res.send("invalid format or image doesn't exist");
+            }
 
-//             });
-//         }
-// 	} else {
-// 		res.status(400);
-// 		res.send('imageUserId must not be empty along with either imageLink or imageDate');
-// 	}
-// });
+		});
+	} else {
+		res.status(400);
+		res.send('imageLink, imageDate, or imageUserId are empty');
+	}
+});
+
+
+app.patch('/4537/API/V1/images/', function(req, res) {
+	connection.query('UPDATE requests SET req_amount = req_amount + 1 WHERE req_name = "images_patch";', function (error, results, fields) {
+		if (error)
+			throw error;
+	});
+
+	let imageLink = req.body.imageLink;
+    let imageDate = req.body.imageDate;
+    let imageUserId = req.body.userId;
+	let newImageLink = req.body.newImageLink;
+	let newImageDate = req.body.newImageDate;
+
+	const query = (column, newValue) => {
+		return "UPDATE images SET " + column + "='" + newValue + "' WHERE images_link='" + imageLink + "' AND images_date='" + imageDate + "' AND id=" + imageUserId + ";";
+	}
+
+	console.error(query("images_date", newImageDate));
+	if ((!(newImageLink == undefined) || !(newImageDate == undefined)) && !(imageUserId== undefined)) {
+        if (!(newImageLink == undefined)) {
+            connection.query(query("images_link", newImageLink), function(error, results, fields) {
+                if (error) {
+                    throw error;
+                }
+
+                if (results) {
+                    res.status(200);
+                    res.send('Update successful');
+                } else {
+                    res.status(401);
+                    res.send('Update failed');
+                }
+				return;
+            });
+        } else if (!(newImageDate == undefined)) {
+			connection.query(query("images_date", newImageDate), function(error, results, fields) {
+                if (error) {
+                    throw error;
+                }
+
+                if (results) {
+                    res.status(200);
+                    res.send('Update successful');
+                } else {
+                    res.status(401);
+                    res.send('Update failed');
+                }
+				return;
+            });
+		}
+	} else {
+		res.status(400);
+		res.send('UserId must not be empty along with either imageLink or imageDate');
+	}
+});
 
 app.listen(port, () => {
 	console.log(`Server running at port: ${port}`);
